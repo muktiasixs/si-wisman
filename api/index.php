3eeadf1bad5d8b52bd1b9f292e4c6f515d4d2887
muktiasixs <dermagani05@gmail.com>
2026-07-27 01:33:55 +0700

    Fix tmp directories creation for vercel

--- api/index.php
+++ api/index.php
@@ -1,5 +1,21 @@
 <?php
 require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
-$app->useStoragePath('/tmp/storage');
+
+$storage = '/tmp/storage';
+$app->useStoragePath($storage);
+
+$directories = [
+    $storage.'/framework/cache/data',
+    $storage.'/framework/views',
+    $storage.'/framework/sessions',
+    $storage.'/logs'
+];
+
+foreach ($directories as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
 $app->handleRequest(Illuminate\Http\Request::capture());
